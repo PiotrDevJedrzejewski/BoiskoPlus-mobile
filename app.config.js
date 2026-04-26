34fcda5afddbef0911c1470c7bb5a76daf8b7d83
PiotrDevJedrzejewski <piotrdevjedrzejewski@gmail.com>
2026-04-26 20:30:53 +0200

    vibe translate from web to mobile

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: 'Boisko+',
     slug: 'boiskoplus',
-    version: '1.0.3',
+    version: '1.0.4',
     orientation: 'portrait',
     icon: './assets/images/boiskoSplash.png',
     scheme: 'boiskoplusmobile',
